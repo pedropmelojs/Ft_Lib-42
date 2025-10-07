5dfb0b6e8da979612caf75f99183f97904b68a68
pedropmelojs <pedroyverdon@gmail.com>
2025-10-07 14:51:07 +0200

    Ft_split retourne pas l'array
    ft_split ne retourne pas l'array via le malloc. A la place c

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,16 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 09:07:32 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/10/07 12:16:08 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/10/07 14:40:20 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // trouver les strings avec end et start comme index dans s
 // le premier et dernier char de chaque string trouvee)
+#include "libft.h"
 #include <stdlib.h>
 
-static int ft_findstart(char const *s, char c, int i)
+static int	ft_findstart(char const *s, char c, int i)
 {
 	while (s[i])
 	{
@@ -48,20 +49,25 @@ char	**ft_split(char const *s, char c)
 	int		start;
 	int		count;
 	char	**ptr;
+	char	**array;
 
+	array = 0;
 	i = 0;
 	count = 0;
 	start = ft_findstart(s, c, i);
-	ptr = malloc(sizeof(*ptr) * count);
-	if (!ptr)
-		return ((void *)0);
 	while (s[i])
 	{
-		ptr[count][0] = s[start];
+		array[count][i] = s[start];
 		if (i == ft_findend(s, c))
-			i = ft_findend(s, c);
-		count++;
+		{
+			array[count][i + 1] = '\0';
+			start = ft_findend(s, c);
+			count++;
+		}
+		i++;
 	}
+	ptr = malloc(sizeof(*array) * count);
+	if (!ptr)
+		return ((void *)0);
 	return (ptr);
 }
-
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:01:20 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/10/07 12:05:39 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/10/07 14:42:28 by rpinheir         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,6 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
 char	**ft_split(char const *s, char c);
 
 #endif
