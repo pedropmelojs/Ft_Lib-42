274d546f472414e215d225e57f400254e245e675
pedropmelojs <pedroyverdon@gmail.com>
2025-10-09 15:54:43 +0200

    ft_split.c OK

--- ft_split.c
+++ ft_split.c
@@ -3,31 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: rpinheir <rpinheir@student.42lausanne.ch>  #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/07 09:07:32 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/10/08 11:47:28 by rpinheir         ###   ########.fr       */
+/*   Created: 2025-10-09 12:22:53 by rpinheir          #+#    #+#             */
+/*   Updated: 2025-10-09 12:22:53 by rpinheir         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-
-static int	ft_findend(char const *s, char c)
-{
-	int	i;
-
-	i = ft_strchr(s, c) - s;
-	while (s[i] != c && s[i] != '\0')
-	{
-		if (s[i + 1] == c)
-		{
-			return (i);
-		}
-		i++;
-	}
-	return (i - 1);
-}
 
 static int	ft_count(const char *s, char c, int count)
 {
@@ -52,41 +36,68 @@ static int	ft_count(const char *s, char c, int count)
 	return (count);
 }
 
-static void	ft_stringtoptr(const char *s, char c, char **ptr)
+static char	**ft_free(char **ptr, int index)
 {
 	int	i;
-	int	index;
-	int	k;
-	int	start;
 
 	i = 0;
-	k = 0;
-	index = 0;
-	start = ft_strchr(s, c) - s;
-	while (s[i])
+	while (i < index)
 	{
-		ptr[index][k] = s[start];
-		k++;
-		if (i == ft_findend(s, c))
-		{
-			k = 0;
-			start = ft_strchr(s, c) - s;
-			index++;
-		}
+		free(ptr[i]);
 		i++;
 	}
+	free(ptr);
+	return (NULL);
 }
 
-// TODO - Fonction pour compter la taille de chaque string pour le malloc
+static char	*ft_stringmanager(const char *s, char c, int start)
+{
+	int		len;
+	char	*string;
+	int		i;
+
+	len = 0;
+	while (s[start + len] && s[start + len] != c)
+	{
+		len++;
+	}
+	string = ft_calloc(len + 1, sizeof(char));
+	if (!string)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		string[i] = s[start + i];
+		i++;
+	}
+	string[i] = '\0';
+	return (string);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
 	int		count;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	count = ft_count(s, c, 0);
 	ptr = ft_calloc(count + 1, sizeof(char *));
 	if (!ptr)
-		return ((void *)0);
-	ft_stringtoptr(s, c, ptr);
+		return (NULL);
+	while (i < count)
+	{
+		while (s[j] == c)
+			j++;
+		ptr[i] = ft_stringmanager(s, c, j);
+		if (!ptr[i])
+			return (ft_free(ptr, i));
+		while (s[j] && s[j] != c)
+			j++;
+		i++;
+	}
+	ptr[i] = NULL;
 	return (ptr);
 }
